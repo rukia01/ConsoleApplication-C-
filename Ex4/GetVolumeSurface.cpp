6d39816ed131dfe66edff2bab51a1ce013a28cf3
rinera182@gmail.com <rinera182@gmail.com>
2022-04-21 11:45:32 +0900

    new

--- Ex4/GetVolumeSurface.cpp
+++ Ex4/GetVolumeSurface.cpp
@@ -3,6 +3,6 @@ double GetVolumeSurface(double x, double y, double z, double* surface)
 {
 	double volume;
 	volume = x * y * z;
-	*surface = ((x * y) + (x * z) + (y * z)) * 2;
+	*surface = (x * y + x * z + y * z) * 2;
 	return volume;
 }
